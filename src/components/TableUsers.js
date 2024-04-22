7e838d07bba8fd839b0b2ff78992e30a94a2394e
djdjdjjdd <longnguyenqwertyuiop@gmail.com>
2024-04-23 00:12:32 +0700

    phan trang user

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,58 +1,79 @@
 import { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
-//import axios from 'axios';
-import {fetchAllUser} from '../services/UserService'
-const  TableUsers = (props) => {
+import { fetchAllUser } from '../services/UserService';
+import ReactPaginate from 'react-paginate';
 
-    const [listUsers, setListUsers] = useState([])
+const TableUsers = (props) => {
+  const [listUsers, setListUsers] = useState([]);
+  const [totalUsers, setTotalUsers] = useState(0);
+  const [totalPages, setTotalPages] = useState(0)
+  useEffect(() => {
+    getUsers(1);
+  }, []);
 
-    useEffect(() => {
-        // axios.get('https://reqres.in/api/users?page=1').then(data => {
-        //     console.log('check data', data)
-        // })
-        getUsers()
-    }, [])
+  const getUsers = async (page) => {
+    let res = await fetchAllUser(page);
+    console.log('>>>', res.data);
 
-    const getUsers = async () => {
-        let res = await fetchAllUser();
-        console.log('>>> check new res', res)
-        if(res && res.data && res.data.data){
-            setListUsers(res.data.data)
-        }
+    if (res && res.data && res.data.data) {
+      console.log(res);
+      setListUsers(res.data.data);
+      setTotalPages(res.total_pages)
+      setTotalUsers(res.total)
     }
-    console.log(listUsers)
-    return (
-        <>
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>email</th>
-                    <th>first_name</th>
-                    <th>last_name</th>
-                </tr>
-            </thead>
-        <tbody>
-            {listUsers && listUsers.length > 0 &&
-                listUsers.map((item, index) => {
-                    return(
-                        <tr key ={`users - ${index}`}>
-                            <td>{item.id}</td>
-                            <td>{item.email}</td>
-                            <td>{item.first_name}</td>
-                            <td>{item.last_name}</td>
-                        </tr>
-                    )
-                })
-            }
-            
-           
-        </tbody>
-        </Table>
- 
+  };
 
-        </>
-    )
-}
+  const handlePageClick = (event) => {
+    console.log('event lib', event);
+    getUsers(+event.selected + 1);
+  };
+
+  return (
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>email</th>
+            <th>first_name</th>
+            <th>last_name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listUsers &&
+            listUsers.length > 0 &&
+            listUsers.map((item, index) => {
+              return (
+                <tr key={`users - ${index}`}>
+                  <td>{item.id}</td>
+                  <td>{item.email}</td>
+                  <td>{item.first_name}</td>
+                  <td>{item.last_name}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={totalPages}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+      />
+    </>
+  );
+};
 
 export default TableUsers;
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,7 +1,7 @@
 // import axios from "axios"
 import axios from "./customize-axios";
-const fetchAllUser = (props) =>  {
-    return axios.get('/api/users?page=2');
+const fetchAllUser = (page) =>  {
+    return axios.get(`/api/users?page=${page}`);
 }
 
 export {fetchAllUser};
